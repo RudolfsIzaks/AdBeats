33bcbb6f674554d0f44bc57e2e706bb5102e8c2c
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-07 19:55:35 +0300

    dfgf

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDl9XqiYLZNT7-JwIL0d8WxbooZtUkn7yQ",
-    authDomain: "adbeats-e1fd0.firebaseapp.com",
-    projectId: "adbeats-e1fd0",
-    storageBucket: "adbeats-e1fd0.appspot.com",
-    messagingSenderId: "190714582709",
-    appId: "1:190714582709:web:898a37bc35f75c82d250fc",
-    measurementId: "G-S4ZJHEY6R8"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
