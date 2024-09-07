9bcfb1a815e180fbaee4d09c50e4f81e8c72d46a
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-07 19:59:16 +0300

    sdfbg

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -13,6 +13,9 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
+console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
