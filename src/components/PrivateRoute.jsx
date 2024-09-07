54b89925b36f9bd0d4c350fed3860fb0d0391c11
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-07 20:11:48 +0300

    dsfg

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,11 @@
-
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 function PrivateRoute({ children }) {
     const isAuthenticated = localStorage.getItem('adminAuth');
 
-    return isAuthenticated ? children : <Navigate to="/admin/login" />;
+    // Check if the user is authenticated; otherwise, redirect to the correct login path
+    return isAuthenticated ? children : <Navigate to="/okvcvdsdv3wfdzx" />;
 }
 
 export default PrivateRoute;
