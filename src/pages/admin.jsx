825c3709f1ff16ba649b86fb509ed0a15c51f624
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-07 21:19:49 +0300

    xdfggn

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import '../index.css';
 import axios from "axios";
