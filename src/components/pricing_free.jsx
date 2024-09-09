fc814b37da58852552167ae9f22e64705a618755
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-09 10:45:47 +0300

    dsg

--- src/components/pricing_free.jsx
+++ src/components/pricing_free.jsx
@@ -10,10 +10,10 @@ function PricingFree() {
   return (
     <>
       <div className="flex items-center justify-center relative p-10">
-      <div className="flex items-center justify-center h-full w-full absolute z-50 text-center bg-stone-700 bg-opacity-25 bg_blurry">
+      <div className="flex items-center justify-center h-full w-full absolute z-50 text-center bg_blurry">
         <p className="font-montserrat font-black text-white text-headline-1">For FREE!</p>
       </div>
-      <div className="z-0">
+      <div className="z-0 scale-50">
         <div className="flex justify-evenly items-center h-auto">
           <div className="bg-stone-800 py-5 px-10 w-96 min-h-[60dvh] flex flex-col justify-between relative">
             <div>
