bdc11ba37b3ce369795828d90cb0e721026b6e5d
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-10 10:55:08 +0300

    ddd

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -26,6 +26,10 @@ import Equinox from "../assets/Equinox.png";
 import Nike from "../assets/Nike.webp";
 import Nike_logo from "../assets/Nike_logo.svg";
 import comic_splash from "../assets/comic_splash.png";
+import seesaw_1 from "../assets/seesaw_1.webp";
+import seesaw_2 from "../assets/seesaw_2.webp";
+import engagement from "../assets/engagement.png";
+import seesaw_4 from '../assets/seesaw_4.webp';
 
 function Landing() {
   const navigate = useNavigate();
@@ -120,31 +124,8 @@ function Landing() {
             (That allows them to get 50% of online traffic, dirt cheap…)
           </p>
         </div>
-        {/* <div className="flex sm:flex-col items-center gap-5 justify-center  mt-5 mx-32">
-        <div className="p-5 flex items-center gap-5 bg-red-500 red-shadow w-96">
-          <FontAwesomeIcon 
-          icon={faCircleXmark}
-          className="text-blue text-3xl"
-          />
-          <p className="text-white">It’s not their endless budget</p>
-        </div>
-        <div className="p-5 flex items-center gap-5 bg-red-500 red-shadow w-96">
-          <FontAwesomeIcon 
-          icon={faCircleXmark}
-          className="text-blue text-3xl"
-          />
-          <p className="text-white">It’s not their “brand”</p>
-        </div>
-        <div className="p-5 flex items-center gap-5 bg-red-500 red-shadow w-96">
-          <FontAwesomeIcon 
-          icon={faCircleXmark}
-          className="text-blue text-3xl"
-          />
-          <p className="text-white"> It’s not the quality of their product</p>
-        </div>
-      </div> */}
-        <div className="flex items-center justify-center relative min-h-[50dvh] sm:scale-75">
-          <img src={comic_splash} alt="" className="absolute z-0" />
+        <div className="flex items-center justify-center relative min-h-[80dvh] sm:scale-75">
+          <img src={comic_splash} alt="" className="absolute z-0 select-none" />
           <p className="z-10 font-comic w-64 text-2xl font-bold -rotate-12">
             {" "}
             It’s all about grabbing and holding attention!
@@ -216,8 +197,8 @@ function Landing() {
                 Show-Stopper
               </h2>
             </div>
-            </div>
-            <div className="flex flex-col items-center gap-4 mb-20 sm:overflow-x-auto sm:w-full">
+          </div>
+          <div className="flex flex-col items-center gap-4 mb-20 sm:overflow-x-auto sm:w-full">
             <div className="md:flex sm:flex sm:overflow-x-auto flex-nowrap items-center sm:gap-5 md:gap-10 sm:h-[70dvh] md:h-[40dvh] mx-10 sm:w-auto md:w-[110%]">
               {cards.map((q, i) => (
                 <div
@@ -246,6 +227,160 @@ function Landing() {
             </div>
           </div>
         </div>
+        <hr className="h-[1px] bg-stone-500 border-none border-stone-500 my-32" />
+        <div className="flex gap-10 items-center md:mx-32">
+          <div className="w-1/2">
+            <p className="text-blue font-comic text-subheadline-3 my-2 select-none">
+              Take the first step towards transforming your ad performance.
+            </p>
+            <h1 className="text-white font-montserrat font-black text-headline-2 my-5 select-none">
+              Your Ads Can Be Unforgettable
+            </h1>
+            <p className="text-stone-400 select-none">
+              Imagine your ad playing in someone's head long after they've
+              scrolled past it.
+              <br />
+              <br />
+              Custom-tailored music can do just that, according to multiple
+              studies​.
+              <br />
+              <br />
+              <br />
+              So naturally, crafting custom tracks to resonate with the 96% who
+              don't click on your ads, is what we do best.
+              <br />
+              <br />
+              The benefit? Your ads won't just be seen; they'll be remembered
+              and acted upon.
+              <br />
+              <br />
+            </p>
+            <Link
+              to="/qualify"
+              className="text-xl font-black italic font-montserrat text-blue animate-pulse"
+            >
+              Sign up now and experience the AdBeats difference!
+            </Link>
+          </div>
+          <div className="relative flex items-center justify-center w-1/2">
+            <div className="absolute w-80 h-auto bg-stone-900 rounded-md p-3 -rotate-12 z-50">
+              <div className="relative flex items-center justify-center select-none">
+                <img src={seesaw_1} className="z-50 w-72 h-80 rounded-md select-none" />
+                <img
+                  src={seesaw_1}
+                  className="absolute z-0 blur-3xl animate-pulse select-none"
+                />
+              </div>
+            </div>
+            <div className="flex items-center absolute w-80 h-96 bg-stone-800 rounded-md p-3 rotate-12 right-10 z-0">
+              <div className="relative flex items-center justify-center">
+                <img src={AdBeats} className="z-50 w-72  rounded-md" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="h-[1px] bg-stone-500 border-none border-stone-500 my-32" />
+        <div className="flex flex-row-reverse gap-10 items-center md:mx-32">
+          <div className="w-1/2">
+            <p className="text-blue font-comic text-subheadline-3 my-2">
+              Attention is the new currency in the world of advertising.
+            </p>
+            <h1 className="text-white font-montserrat font-black text-headline-2 my-5">
+              Stand Out with Unique, Attention-Grabbing Ads
+            </h1>
+            <p className="text-stone-400">
+              <b>Why make ads that are easily overlooked? </b>
+              <br />
+              <br />
+              AdBeats offers a fresh approach to your brand's copywriting and
+              tone, guaranteeing that your ads command attention.
+              <br />
+              <br />
+              Research indicates that ads with branded audio are 44% more
+              noticed and engaging​.
+              <br />
+              <br />
+              Adbeats doesn't just create a track for your brand, but a track
+              meant to persuade! The result? Ads that can't be ignored.
+            </p>
+          </div>
+          <div className="relative flex items-center justify-center w-1/2">
+            <div className="flex items-center justify-center relative">
+              <img src={seesaw_2} className="w-3/5 rounded-md z-50" />
+              <img
+                src={seesaw_2}
+                className="absolute w-3/5 z-0 blur-3xl animate-pulse"
+              />
+            </div>
+          </div>
+        </div>
+        <hr className="h-[1px] bg-stone-500 border-none border-stone-500 my-32" />
+        <div className="flex gap-10 items-center md:mx-32">
+          <div className="w-1/2">
+            <p className="text-blue font-comic text-subheadline-3 my-2">
+              Transform Mere Attention into Laser-Focused Interest…
+            </p>
+            <h1 className="text-white font-montserrat font-black text-headline-2 my-5">
+              Boost Engagement and Retention, Instantly
+            </h1>
+            <p className="text-stone-400">
+              Engagement is the lifeblood of successful advertising.
+              <br />
+              <br />
+              So naturally, who wouldn’t want a stunning 64% increase in
+              engagement?
+              <br />
+              <br />
+              We’re talking floods of comments, shares, and genuine interest.
+              <br />
+              <br />
+              And the best part?
+              <br />
+              <br />
+              Advertising platforms reward engagement with drastically lower
+              costs- exponentially boosting your ROI!
+            </p>
+          </div>
+          <div className="relative flex items-center justify-center w-1/2">
+            <div className="flex items-center justify-center relative">
+              <img src={engagement} className="rounded-md z-50" />
+              <img src={engagement} className="absolute rounded-md z-0" />
+            </div>
+          </div>
+        </div>
+        <hr className="h-[1px] bg-stone-500 border-none border-stone-500 my-32" />
+        <div className="flex flex-row-reverse gap-10 items-center md:mx-32">
+          <div className="w-1/2">
+            <h1 className="text-white font-montserrat font-black text-headline-2 my-5">
+              Create Timeless Ads that Defy Ad Fatigue
+            </h1>
+            <p className="text-stone-400">
+              Ad fatigue is a common challenge, but it’s not insurmountable.
+              <br />
+              <br />
+              The key to drowning ad fatigue and prolonging your campaign's
+              success is engagement.
+              <br />
+              <br />
+              AdBeats excels in creating timeless audio that keeps your ads
+              fresh and engaging.
+              <br />
+              <br />
+              The benefit?
+              <br />
+              <br />
+              Prolonged ad performance and sustained audience interest, ensuring
+              your marketing efforts yield long-term results.
+            </p>
+          </div>
+          <div className="relative flex items-center justify-center w-1/2">
+            <div className="flex items-center justify-center relative p-5 bg-stone-800 rounded-md -rotate-6">
+              <img src={seesaw_4} className="rounded-md w-96 z-50" />
+              <img src={seesaw_4} className="absolute rounded-md z-0 blur-3xl animate-pulse" />
+            </div>
+          </div>
+        </div>
+        <hr className="h-[1px] bg-stone-500 border-none border-stone-500 my-32" />
       </div>
     </>
   );
