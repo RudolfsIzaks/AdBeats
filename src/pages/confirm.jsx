d6e61f61c8ee2f936d6f3b4fb4b8c2f188cfb359
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-10 17:34:35 +0300

    44

--- src/pages/confirm.jsx
+++ src/pages/confirm.jsx
@@ -9,40 +9,6 @@ function Confirm() {
   const [searchParams] = useSearchParams();
   const orderId = location.state?.orderId || searchParams.get("orderId"); // Fetch from location or query params
 
-  const [formData, setFormData] = useState({
-    email: "",
-    order_id: orderId,
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSendOut = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        "https://aqueous-tor-91749-7319d44de38a.herokuapp.com/auto-mail-order",
-        formData
-      );
-
-      // Reset form data after successful submission
-      setFormData({
-        email: "",
-        order_id: orderId,
-      });
-
-      console.log(response);
-
-      // Navigate to the "done" page after success
-      navigate("/done");
-    } catch (error) {
-      console.error("Error submitting the form:", error);
-    }
-  };
 
   useEffect(() => {
     if (!orderId) {
@@ -57,30 +23,14 @@ function Confirm() {
           <h1 className="text-blue font-comic font-bold sm:text-subheadline-3 md:text-headline-2">
             {orderId ? "Your Order is Confirmed!" : "No Order Found!"}
           </h1>
-          <p className="text-white font-montserrat sm:text-sm md:text-subheadline-4 text-center">
+          <p className="text-white font-montserrat sm:text-sm md:text-subheadline-4">
             Your Order ID is: {orderId}
           </p>
-          <form onSubmit={handleSendOut} className="flex flex-col items-center gap-10">
-            <div className="flex sm:flex-col gap-5 items-center">
-              <label htmlFor="email" className="text-white font-montserrat text-lg">
-                Your email to receive Order ID:
-              </label>
-              <input
-                type="email"
-                required
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="bg-transparent outline-none border-b border-b-blue text-white"
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-blue comic_btn font-comic sm:text-lg md:text-2xl font-bold px-5 py-2 border hover:bg-black border-blue transition hover:text-blue hover:scale-110"
-            >
-              Send ID
-            </button>
-          </form>
+          <p className="text-stone-300 font-comic md:text-lg">
+            We have recieved your order and are working on your song right now! <br /><br /> 
+            We will send you an email when the song is complete and ready to be claimed.
+          </p>
+          <Link className="py-2 px-5 bg-red-500 text-white font-comic font-bold text-center red-shadow" to="/">Continue Browsing</Link>
         </div>
       </div>
     </>
