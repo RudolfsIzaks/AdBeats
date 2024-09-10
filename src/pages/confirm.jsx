74c77a0789e596bd9eb2fd5cd8248a94b4681609
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-10 16:18:35 +0300

    sdfgdd

--- src/pages/confirm.jsx
+++ src/pages/confirm.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "../index.css";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 
 function Confirm() {
   const navigate = useNavigate();
   const location = useLocation();
-  const apiResponse = location.state?.orderId || location.state?.response; // Handle both free and paid cases
+  const [searchParams] = useSearchParams();
+  const orderId = location.state?.orderId || searchParams.get("orderId"); // Fetch from location or query params
 
   const [formData, setFormData] = useState({
     email: "",
-    order_id: apiResponse,
+    order_id: orderId,
   });
 
-  // Handle form changes
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -21,7 +21,6 @@ function Confirm() {
     });
   };
 
-  // Send email with the order ID after confirmation
   const handleSendOut = async (e) => {
     e.preventDefault();
     try {
@@ -33,7 +32,7 @@ function Confirm() {
       // Reset form data after successful submission
       setFormData({
         email: "",
-        order_id: apiResponse,
+        order_id: orderId,
       });
 
       console.log(response);
@@ -46,23 +45,22 @@ function Confirm() {
   };
 
   useEffect(() => {
-    // You can check for the order_id in case of a stripe payment
-    if (!apiResponse) {
-      navigate("/"); // Redirect to homepage if no order_id is found
+    if (!orderId) {
+      navigate("/"); // Redirect to homepage if no orderId is found
     }
-  }, [apiResponse, navigate]);
+  }, [orderId, navigate]);
 
   return (
     <>
       <div className="flex items-center justify-center my-10">
         <div className="p-10 flex flex-col gap-5 bg-stone-800 border-stone-600">
           <h1 className="text-blue font-comic font-bold sm:text-subheadline-3 md:text-headline-2">
-            {apiResponse ? "Your Order is Confirmed!" : "No Order Found!"}
+            {orderId ? "Your Order is Confirmed!" : "No Order Found!"}
           </h1>
           <p className="text-white font-montserrat sm:text-sm md:text-subheadline-4 text-center">
-            Your Order ID is: {apiResponse}
+            Your Order ID is: {orderId}
           </p>
-          <form onSubmit={handleSendOut} action="submit" className="flex flex-col items-center gap-10">
+          <form onSubmit={handleSendOut} className="flex flex-col items-center gap-10">
             <div className="flex sm:flex-col gap-5 items-center">
               <label htmlFor="email" className="text-white font-montserrat text-lg">
                 Your email to receive Order ID:
