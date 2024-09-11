71dd959d8a3f440b9b25f7bcc893d745c57e5006
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-11 10:39:39 +0300

    33

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,17 +11,19 @@ function Footer() {
          <hr className="bg-stone-400 h-0 border-stone-700" />
          <nav className="flex justify-between p-10">
             <img src={AdBeats} className="w-1/6" />
-            <div className="flex flex-col items-center gap-2 flex-grow">
-                <Link className="underline text-stone-300 text-left md:w-96" to="/faq">FAQs</Link>
-                <Link className="underline text-stone-300 text-left md:w-96" to="/privacy">Privacy Policy</Link>
-                <Link className="underline text-stone-300 text-left md:w-96" to="/terms">Terms & Conditions</Link>
-                <Link className="underline text-stone-300 text-left md:w-96" to="/">Homepage</Link>
+            <div className="flex gap-10 flex-grow">
+            <div className="flex flex-col items-center gap-2">
+                <Link className="text-stone-300 text-left md:w-96" to="/faq">FAQs</Link>
+                <Link className="text-stone-300 text-left md:w-96" to="/privacy">Privacy Policy</Link>
+                <Link className="text-stone-300 text-left md:w-96" to="/terms">Terms & Conditions</Link>
+                <Link className="text-stone-300 text-left md:w-96" to="/">Homepage</Link>
             </div>
-            <div className="flex flex-col items-center gap-2 flex-grow">
-                <Link className="underline text-stone-300 text-left md:w-96" to="/partners">Helping Hands</Link>
-                <Link className="underline text-stone-300 text-left md:w-96" to="/contanct-help">Contact For Help</Link>
-                <Link className="underline text-stone-300 text-left md:w-96" to="/affiliates">Affiliate Program</Link>
-                <Link className="underline text-stone-300 text-left md:w-96" to="/qualify">Purchase AdBeats Plan</Link>
+            <div className="flex flex-col items-center gap-2">
+                <Link className="text-stone-300 text-left md:w-96" to="/partners">Helping Hands</Link>
+                <Link className="text-stone-300 text-left md:w-96" to="/contanct-help">Contact For Help</Link>
+                <Link className="text-stone-300 text-left md:w-96" to="/affiliates">Affiliate Program</Link>
+                <Link className="text-stone-300 text-left md:w-96" to="/qualify">Purchase AdBeats Plan</Link>
+            </div>
             </div>
          </nav>
          <hr className="bg-stone-400 h-0 border-stone-700" />
