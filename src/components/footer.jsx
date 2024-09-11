58d5d6f2aebc179bffac5a0a1d1854d30cb8f442
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-11 10:37:52 +0300

    footer adjustment

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,16 +12,16 @@ function Footer() {
          <nav className="flex justify-between p-10">
             <img src={AdBeats} className="w-1/6" />
             <div className="flex flex-col items-center gap-2 flex-grow">
-                <Link className="underline text-stone-300" to="/faq">FAQs</Link>
-                <Link className="underline text-stone-300" to="/privacy">Privacy Policy</Link>
-                <Link className="underline text-stone-300" to="/terms">Terms & Conditions</Link>
-                <Link className="underline text-stone-300" to="/">Homepage</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/faq">FAQs</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/privacy">Privacy Policy</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/terms">Terms & Conditions</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/">Homepage</Link>
             </div>
             <div className="flex flex-col items-center gap-2 flex-grow">
-                <Link className="underline text-stone-300" to="/partners">Helping Hands</Link>
-                <Link className="underline text-stone-300" to="/contanct-help">Contact For Help</Link>
-                <Link className="underline text-stone-300" to="/affiliates">Affiliate Program</Link>
-                <Link className="underline text-stone-300" to="/qualify">Purchase AdBeats Plan</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/partners">Helping Hands</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/contanct-help">Contact For Help</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/affiliates">Affiliate Program</Link>
+                <Link className="underline text-stone-300 text-left md:w-96" to="/qualify">Purchase AdBeats Plan</Link>
             </div>
          </nav>
          <hr className="bg-stone-400 h-0 border-stone-700" />
