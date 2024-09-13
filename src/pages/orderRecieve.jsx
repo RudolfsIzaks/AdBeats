9b10e3b384b84734d428036a8b8e1793e1357dd2
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-13 11:06:27 +0300

    srgr

--- src/pages/orderRecieve.jsx
+++ src/pages/orderRecieve.jsx
@@ -86,7 +86,7 @@ function OrderRecieve() {
           </nav>
           <div className="w-screen h-[1px] hr-gradient"></div>
         </div>
-        <div className="min-h-[100dvh] flex items-end justify-center z-10 bg-backgroundblack relative">
+        <div className="h-[100dvh] flex items-center justify-center z-10 bg-backgroundblack relative">
           <div className="w-2/3 flex flex-col items-center">
             <p className="font-comic text-blue text-center text-2xl">
               If you got an email that your order is ready
