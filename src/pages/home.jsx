5ec8a17c5d158bc5959f89553bb3ef1ec892628b
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-17 11:21:08 +0300

    segsdf

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -329,7 +329,7 @@ function Home() {
             <img
               src={questions[currentQuestion].sideImg}
               alt=""
-              className="h-full md:w-1/2 sm:hidden rounded-r-md"
+              className="h-full sm:hidden rounded-r-md"
             />
             <form
               onSubmit={handleSubmit}
