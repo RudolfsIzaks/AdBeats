585c477e063a3a933c15ded7654bd1c884b9aadc
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-09-27 11:00:44 +0300

    ddd

--- src/components/routing.jsx
+++ src/components/routing.jsx
@@ -14,6 +14,7 @@ import Contact from "../support/contact";
 import ThanksSupport from "../support/thanks-contact";
 import PrivacyPolicy from "../support/privacy";
 import TermsConditions from "../support/terms";
+import Cancel from "../pages/cancel";
 
 function RouterSetup() {
   return (
@@ -35,6 +36,7 @@ function RouterSetup() {
         <Route path="/pricing" element={<Pricing />}/>
         <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
         <Route path="/terms-conditions" element={<TermsConditions />}/>
+        <Route path="/cancel" element={<Cancel />}/>
         <Route
           path="/admin/dashboard"
           element={
